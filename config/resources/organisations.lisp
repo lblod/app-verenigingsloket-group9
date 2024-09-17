1d5ce172bf052d7042e9d18c2c6b4b292f94b090
Erika Pauwels <erika.pauwels@gmail.com>
2024-09-17 10:01:16 +0200

    Configure data model for submissions

--- config/resources/organisations.lisp
+++ config/resources/organisations.lisp
@@ -2,6 +2,9 @@
   :class (s-prefix "besluit:Bestuurseenheid")
   :properties `((:name :string ,(s-prefix "skos:prefLabel"))
                  (:classification :url ,(s-prefix "besluit:classificatie")))
+  :has-many `((submission :via ,(s-prefix "omgeving:Rechtshandeling.verantwoordelijke")
+                :as "submissions"
+                :inverse t))
   :resource-base (s-url "http://data.lblod.info/id/bestuurseenheden/")
   :features '(include-uri)
   :on-path "administrative-units"
@@ -11,8 +14,8 @@
   :class (s-prefix "org:Organization")
   :properties `((:name :string ,(s-prefix "skos:prefLabel")))
   :has-many `((user :via ,(s-prefix "foaf:member")
-                :as "members")
+                :as "members"))
   :resource-base (s-url "http://data.lblod.info/id/organisations/")
   :features '(include-uri)
-  :on-path "administrative-units"
+  :on-path "organizations"
 )
